4ea85193df4c9264fd013d90ebd4cb4e5dce64d7
ved603 <gujjuvedant603@gmail.com>
2024-05-11 13:10:32 +0530

    Invoice calculator method added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
 public class Main {
+    private static final int COST_PER_KM = 10;
+    private static final int COST_PER_MINUTE = 1;
+    private static final int MINIMUM_FARE = 5;
+    public static double calculateFare(double distanceKm, int timeMinutes) {
+        double fare = distanceKm * COST_PER_KM + timeMinutes * COST_PER_MINUTE;
+        fare = Math.max(fare, MINIMUM_FARE);
+        return fare;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double distance = 0.5;
+        int time = 1;
+        double totalFare = calculateFare(distance, time);
+        System.out.println("Total fare: Rs " + totalFare);
     }
 }
