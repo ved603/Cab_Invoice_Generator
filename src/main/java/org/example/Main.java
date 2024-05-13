ae54fe74027625c099c4a7f01d2c80326cb63797
ved603 <gujjuvedant603@gmail.com>
2024-05-13 14:39:56 +0530

    Data can be retrived by id

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,42 +1,97 @@
 package org.example;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Main {
+    HashMap<Integer,Generator> hm = new HashMap<>();
+    public String method(int id,double [] distance,int [] time){
+        try {
+            hm.put(id, new Generator(distance, time));
+            return "Bill Generated";
+        }
+        catch(Exception e){
+            return "Bill not Generated";
+        }
+    }
+    public String printmethod(int id){
+        try{
+            if(hm.containsKey(id)){
+                System.out.println("\n\n_____________________________________________________________________\n");
+                System.out.println("\t\t\t\t\t** Invoice Generator **\t\t");
+                System.out.println("______________________________________________________________________");
+                System.out.println("\nThe Id of the person is  " + id);
+                System.out.println("Total Number of rides is " +hm.get(id).getRides());
+                System.out.println("Total Fare is " + hm.get(id).getTotal());
+                System.out.println("Average Fare per Ride is " + hm.get(id).getAvg());
+                System.out.println("\n_______________________________________________________________________\n");
+
+                return "Success";
+            }
+
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+
+        }
+        return "Failed";
+        }
+
+}
+
+
+class Generator{
+
     private static final int COST_PER_KM = 10;
     private static final int COST_PER_MINUTE = 1;
     private static final int MINIMUM_FARE = 5;
 
     ArrayList<Double> arr = new ArrayList<>();
-    double avg;
-    int Total;
+    double total =0.0;
+    double avg = 0.0;
+    int rides = 0;
+
+    public Generator(double[] distance, int[] time) {
+        int n = Math.max(distance.length, time.length);
+        for (int i = 0; i < n; i++) {
+            double totalFare = calculateFare(distance[i], time[i]);
+            arr.add(totalFare);
+        }
+        double Total=0.0;
+        for (Double tot : arr) {
+            Total += tot;
+        }
+        avg = Total / arr.size();
+        setAvg(avg);
+        setTotal(Total);
+        setRides(n);
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public void setAvg(double avg) {
+        this.avg = avg;
+    }
+
+    public void setRides(int rides) {
+        this.rides = rides;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public double getAvg() {
+        return avg;
+    }
+
+    public int getRides() {
+        return rides;
+    }
     public static double calculateFare(double distanceKm, int timeMinutes) {
         double fare = distanceKm * COST_PER_KM + timeMinutes * COST_PER_MINUTE;
         fare = Math.max(fare, MINIMUM_FARE);
         return fare;
-    }
-    public String Invoice(double [] distance, int [] time){
-        int n = Math.max(distance.length, time.length);
-        for(int i=0;i<n;i++) {
-            if(distance[i] == 0 || time[i] == 0){
-                return "Failed";
-            }
-            double totalFare = calculateFare(distance[i], time[i]);
-            arr.add(totalFare);
-        }
-        for (Double tot : arr) {
-            Total += tot;
-        }
-
-        avg = (double) Total/arr.size();
-
-        System.out.println("\n\n_____________________________________________________________________\n");
-        System.out.println("\t\t\t\t\t** Invoice Generator **\t\t");
-        System.out.println("______________________________________________________________________");
-        System.out.println("\nTotal Number of rides is " +n);
-        System.out.println("Total Fare is " +Total);
-        System.out.println("Average Fare per Ride is " +avg);
-        System.out.println("\n_______________________________________________________________________\n");
-
-        return "Success";
     }
 }
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -8,18 +8,19 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 public class MainTest {
     Main m = new Main();
-    @ParameterizedTest
+    @ParameterizedTest(name = "Passing the id and value also retriving the data by id")
     @MethodSource("arraysProvider")
-    public void invo(double [] distance,int [] time){
-        Assert.assertEquals("Success", m.Invoice(distance,time));
+    public void invo(int id,double [] distance,int [] time){
+        Assert.assertEquals("Bill Generated", m.method(id,distance,time));
+        Assert.assertEquals("Success",m.printmethod(id));
     }
     static Stream<Arguments> arraysProvider() {
         return Stream.of(
-                Arguments.of(new double[]{1.0, 2.1, 3.4,5.4}, new int[]{1, 2, 3,1}), // Test case 1: Equal arrays
-                Arguments.of(new double[]{0.5,0.3,1}, new int[]{2,1,4}) // Test case 2: Empty arrays
-//                Arguments.of(new double[]{1, 2, 3}, new int[]{3, 2, 1}), // Test case 3: Arrays with same elements but in reverse order
-//                Arguments.of(new double[]{1, 2, 3}, new int[]{1, 2}), // Test case 4: Arrays with different lengths
-//                Arguments.of(new double[]{1, 2, 3}, new int[]{1, 2, 4}) // Test case 5: Arrays with different elements
+                Arguments.of(1, new double[]{1.0, 2.1, 3.4,5.4}, new int[]{1, 2, 3,1}),
+                Arguments.of(2,new double[]{0.5,0.3,1}, new int[]{2,1,4}),
+                Arguments.of(3,new double[]{7,8.5,3.5}, new int[]{22,60,5}),
+                Arguments.of(4,new double[]{5.5,4.3,6.7,8.7,3.2,1.25,10.32}, new int[]{30,23,60,75,20,11,100}),
+                Arguments.of(5,new double[]{1, 2, 3}, new int[]{1, 2, 4})
         );
     }
 }
