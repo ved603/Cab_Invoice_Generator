9f40c6e0de8914231e7fb5bf8e2ead621a79fee8
ved603 <gujjuvedant603@gmail.com>
2024-05-13 15:01:45 +0530

    Supports Premium Rides

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -20,9 +20,13 @@ public class Main {
                 System.out.println("\t\t\t\t\t** Invoice Generator **\t\t");
                 System.out.println("______________________________________________________________________");
                 System.out.println("\nThe Id of the person is  " + id);
-                System.out.println("Total Number of rides is " +hm.get(id).getRides());
-                System.out.println("Total Fare is " + hm.get(id).getTotal());
-                System.out.println("Average Fare per Ride is " + hm.get(id).getAvg());
+                System.out.println("\nTotal Number of rides is " +hm.get(id).getRides());
+                System.out.println("Total Fare for Premium Rides is " + hm.get(id).getnormalTotal());
+                System.out.println("Average Fare for Premium per Ride is " + hm.get(id).getnormalAvg());
+
+                System.out.println("\nTotal Number of rides is " +hm.get(id).getRides());
+                System.out.println("Total Fare for Normal Rides is " + hm.get(id).getPremium_total());
+                System.out.println("Average Fare for Normal per Ride is " + hm.get(id).getPremium_avg());
                 System.out.println("\n_______________________________________________________________________\n");
 
                 return "Success";
@@ -45,53 +49,95 @@ class Generator{
     private static final int COST_PER_MINUTE = 1;
     private static final int MINIMUM_FARE = 5;
 
+    private static final int COST_PER_KM_pre = 15;
+    private static final int COST_PER_MINUTE_pre = 2;
+    private static final int MINIMUM_FARE_pre = 20;
+
     ArrayList<Double> arr = new ArrayList<>();
-    double total =0.0;
-    double avg = 0.0;
+    ArrayList<Double> pre_arr = new ArrayList<>();
+    double normal_total =0.0;
+    double normal_avg = 0.0;
+
+    double premium_total = 0.0;
+    double premium_avg = 0.0;
     int rides = 0;
 
     public Generator(double[] distance, int[] time) {
         int n = Math.max(distance.length, time.length);
         for (int i = 0; i < n; i++) {
-            double totalFare = calculateFare(distance[i], time[i]);
+            double totalFare = calculateFareNormal(distance[i], time[i]);
             arr.add(totalFare);
         }
         double Total=0.0;
         for (Double tot : arr) {
             Total += tot;
         }
-        avg = Total / arr.size();
-        setAvg(avg);
-        setTotal(Total);
+        normal_avg = Total / arr.size();
+        setnormalAvg(normal_avg);
+        setnormalTotal(Total);
         setRides(n);
+
+
+
+        for (int i = 0; i < n; i++) {
+            double totalFare = calculateFarePremium(distance[i], time[i]);
+            pre_arr.add(totalFare);
+        }
+        double total_pre = 0.0;
+        for (Double tot : pre_arr) {
+           total_pre = tot;
+        }
+        premium_avg = total_pre / pre_arr.size();
+        setPremium_avg(premium_avg);
+        setPremium_total(total_pre);
     }
 
-    public void setTotal(double total) {
-        this.total = total;
+    public void setnormalTotal(double total) {
+        this.normal_total = total;
     }
 
-    public void setAvg(double avg) {
-        this.avg = avg;
+    public void setnormalAvg(double avg) {
+        this.normal_avg = avg;
     }
 
     public void setRides(int rides) {
         this.rides = rides;
     }
 
-    public double getTotal() {
-        return total;
+    public void setPremium_avg(double avg){
+        this.premium_avg = avg;
+    }
+    public void setPremium_total(double total){
+        this.premium_total = total;
+    }
+    public double getnormalTotal() {
+        return normal_total;
     }
 
-    public double getAvg() {
-        return avg;
+    public double getnormalAvg() {
+        return normal_avg;
     }
 
+    public double getPremium_total(){
+        return premium_total;
+    }
+
+    public double getPremium_avg(){
+        return premium_avg;
+    }
     public int getRides() {
         return rides;
     }
-    public static double calculateFare(double distanceKm, int timeMinutes) {
+    public static double calculateFareNormal(double distanceKm, int timeMinutes) {
         double fare = distanceKm * COST_PER_KM + timeMinutes * COST_PER_MINUTE;
         fare = Math.max(fare, MINIMUM_FARE);
         return fare;
     }
+
+    public static double calculateFarePremium(double distanceKm, int timeMinutes) {
+        double fare = distanceKm * COST_PER_KM_pre + timeMinutes * COST_PER_MINUTE_pre;
+        fare = Math.max(fare, MINIMUM_FARE_pre);
+        return fare;
+    }
+
 }
